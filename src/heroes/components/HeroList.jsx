6fb74974d8d16b7ab67dde1ebc69d3e43ff96cce
ehalfonso <ehalfonso92@gmail.com>
2024-07-04 01:00:55 -0700

    create hero cards

---
+++ src/heroes/components/HeroList.jsx
@@ -0,0 +1,13 @@
+import { HeroCard } from "./HeroCard";
+import { getHeroByPublisher } from "../helpers";
+
+export const HeroList = ({ publisher }) => {
+  const heroes = getHeroByPublisher(publisher);
+  return (
+    <div className="row rows-cols-1 row-cols-md-3 g-3">
+      {heroes?.map((heroe) => (
+        <HeroCard key={heroe.id} {...heroe} />
+      ))}
+    </div>
+  );
+};
