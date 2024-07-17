c4515fe9338de9ffb247950281b0a69c13c14c4e
ehalfonso <ehalfonso92@gmail.com>
2024-07-16 22:33:55 -0700

    Search finished

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -32,6 +32,7 @@ export const Navbar = () => {
           >
             DC
           </NavLink>
+
           <NavLink
             className={({ isActive }) =>
               `nav-item nav-link ${isActive ? "active" : ""}`
